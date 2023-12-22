b3bfc1c9c9525a9cf4404c61f8072a6e199a7180
Poojabhardwaj98766 <104709618+Poojabhardwaj98766@users.noreply.github.com>
2023-12-22 20:18:57 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,49 @@
+const progressBar = document.getElementById("progress-bar");
+// Update the progress (e.g., set it to 50%)
+progressBar.style.width = "50%";
+function toggleMenu() {
+    var navList = document.querySelector('.nav-list');
+    navList.classList.toggle('show');
+}
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
+
+    });
+});
+
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
